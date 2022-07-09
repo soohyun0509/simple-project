2cf00ed448680be35eeddf30adf791e32fb330c6
soohyun0509 <soohyun0509@naver.com>
2022-07-09 21:31:14 +0900

    second

---
+++ time/main.js
@@ -0,0 +1,26 @@
+// 시간, 분, 초, AM 설정해야함
+// 12시간으로 해야함
+// 시간 흐르게...
+// 이걸 HTMl에 안넣고 어떻게 화면에 어떻게 나오게하지...
+
+
+// const time= new Date();
+// const hour= time.getHours();
+// const minute= time.getMinutes();
+// const second= time.getSeconds();
+let set= "AM";
+
+function showTime() {
+    const time= new Date();
+    const hour= time.getHours();
+    const minute= time.getMinutes();
+    const second= time.getSeconds();
+    console.log(hour);
+}
+
+showTime();
+
+function show() {
+    const github= gaeppackcu
+}
+
